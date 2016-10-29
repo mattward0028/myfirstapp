232ec9d40b4e416f30b62fb30428b593d242d1ad
mattward0028 <mattward0028@gmail.com>
2016-10-28 17:19:48 -0700

    5.3 added contact form and enabled heroku email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,14 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'mattward0028@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
