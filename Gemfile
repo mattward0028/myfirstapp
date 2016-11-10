cfe6ef1a16103548c4ddfc1c684cef07ae54e68c
mattward0028 <mattward0028@gmail.com>
2016-11-09 18:19:41 -0800

    6.5 Installed stripe

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'will_paginate', '~> 3.1.0'
 gem 'rails-controller-testing'
 
 gem "factory_girl_rails", "~> 4.0"
+
+gem 'stripe'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103235652) do
+ActiveRecord::Schema.define(version: 20161110013308) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 20161103235652) do
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "colour"
-    t.decimal  "price"
+    t.decimal  "price_in_cents"
   end
 
   create_table "users", force: :cascade do |t|
