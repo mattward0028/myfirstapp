9dba3f1ccca0b643247dfdf4572063d67ea5e4cc
mattward0028 <mattward0028@gmail.com>
2016-11-12 16:26:56 -0800

    6.6 Generated payments controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ end
 
   post 'static_pages/thank_you'
 
+  post 'payments/create'
+
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
