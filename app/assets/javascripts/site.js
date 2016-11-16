3b23cc2210018d76a4f02945142bbf058fff90ec
mattward0028 <mattward0028@gmail.com>
2016-11-16 13:45:24 -0800

    6.10 Learned to used AJAX with comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,15 @@
-$(document).on('turbolinks:load', function(){
-    $('.img-zoom').elevateZoom();
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
+};
+
+$(document).on('turbolinks:load', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
 });
