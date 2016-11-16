6a3a075479e8712577573a3962a6a9423ef1e1ae
mattward0028 <mattward0028@gmail.com>
2016-11-15 16:13:59 -0800

    6.5 testing stripe integer issues

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,7 @@ class PaymentsController < ApplicationController
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
-      :amount => @product.price.to_i*100, # amount in cents, again
+      :amount => @product.price_in_cents, # amount in cents, again
       :currency => "usd",
       :source => token,
       :description => params[:stripeEmail]
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
   resources :products do
   resources :comments
+  resources :payments, only: [:create]
 end
   resources :users
   get 'static_pages/about'
