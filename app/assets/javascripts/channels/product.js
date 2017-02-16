1c089d4e15b2e7bd6a5c4fb530c82d0521731a80
mattward0028 <mattward0028@gmail.com>
2017-02-15 17:09:48 -0800

    6.14

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,5 +1,7 @@
 App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
+    console.log('connected');
+    this.listen_to_comments();
     // Called when the subscription is ready for use on the server
   },
 
@@ -9,6 +11,21 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    $(".alert.alert-info").show();
-  }
+  $(".alert.alert-info").show();
+  $('.product_reviews').prepend(data.comment);
+  $("#average-rating").attr('data-score', data.average_rating);
+  refreshRating();
+},
+  
+
+  listen_to_comments: function () {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
+}
+
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
